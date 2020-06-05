b9acf7007281cf201936c9f0abf838cf90d82d17
Creamy <recklessboy81@outlook.com>
2020-06-05 01:22:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ client.on('message', message => {
         let Embed = new Discord.MessageEmbed()
         .setColor(`#33FFAF`)
         .setTitle("extra roles")
-        .setThumbnail({files :["./images/kanna1.png"]})
+        .setThumbnail('https://cdn.discordapp.com/attachments/706297283074850878/718379154109890560/kanna1.png')
         .addField("Roles", ":paintbrush: **Artist** This role will show that your an amazing artist!", true);
         message.channel.send({embed: Embed});}
     
